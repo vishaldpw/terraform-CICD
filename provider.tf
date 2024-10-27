fe41ca81a3283081d9a46220b17ada994855f638
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-10-27 14:21:17 +0530

    First Commit

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
 
-
-provider "aws" {
-  region     = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
