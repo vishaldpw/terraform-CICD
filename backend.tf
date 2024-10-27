a00d2dd6c449ae537cbd67516455f837e72529f7
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-10-27 19:04:38 +0530

    First Commit

--- backend.tf
+++ backend.tf
@@ -1,8 +1,19 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-statefile-2710"
+#     key    = "production/ec2"
+#     dynamodb_table = "tadap-table"
+#     region = "us-east-2"
+#   }
+# }
+
+
+# backend.tf
 terraform {
   backend "s3" {
-    bucket = "terraform-statefile-2710"
-    key    = "production/ec2"
-    dynamodb_table = "tadap-table"
-    region = "us-east-2"
+    bucket         = "terraform-statefile-2710"  # Your S3 bucket name
+    key            = "production/ec2"           # Unique key for the state file
+    dynamodb_table = "tadap-table"              # DynamoDB table for state locking
+    region         = "us-east-2"                # Region for S3 and DynamoDB
   }
 }
