4123558831edc797a2f1d66950fd029958a288b0
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-10-27 21:05:31 +0530

    latest Change

--- backend.tf
+++ backend.tf
@@ -1,12 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-statefile-2710"
-#     key    = "production/ec2"
-#     dynamodb_table = "tadap-table"
-#     region = "us-east-2"
-#   }
-# }
-
 
 # backend.tf
 terraform {
