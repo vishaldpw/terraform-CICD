4aeb7a7ebb225f20715d16d0099941a2b334eac3
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-10-28 17:16:53 +0530

    28-10-2024

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ module "ec2_instances" {
   app            = "stackstorm"
   app2           = "stackstorm-db"
 }
+
+module "ec2_instances" {
+  source         = "./modules/ec2"
+  ami            = "ami-00eb69d236edcfaf8"
+  instance_type  = "t2.micro"
+  instance-name  = "Vishal-test-app2"
+  instance-name2 = "Vishal-test-db2"
+  app            = "stackstorm"
+  app2           = "stackstorm-db"
+}
