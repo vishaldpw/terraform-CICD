7824dd38ccde643ebe6d223765807fc1cf233ea9
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-11-06 18:52:01 +0530

    added VPC module

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   name   = "my-vpc-2"
   cidr   = "10.0.0.0/16"
 
-  azs             = ["eu-east-2a", "eu-east-2b", "eu-east-2c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
