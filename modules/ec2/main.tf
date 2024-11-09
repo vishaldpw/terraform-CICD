fc2ee24311034780160bbd1cf7bade3a5cd4238a
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-11-09 14:37:45 +0530

    ansible

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "my-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = "black-Bottle"
   tags = {
     Name = var.instance-name
     application = var.app
@@ -10,6 +11,7 @@ resource "aws_instance" "my-ec2" {
 resource "aws_instance" "my-ec2-2" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = "black-Bottle"
   tags = {
     Name = var.instance-name2
     application = var.app2
