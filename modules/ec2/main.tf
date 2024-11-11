461623eb6847d94ae96384ec2d71876dfd3a2b4c
[Vishal Chauhan] <[vishal.singh-chauhan@capgemini.com]>
2024-11-11 14:30:40 +0530

    vpc

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,18 +2,10 @@ resource "aws_instance" "my-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name = "black-Bottle"
+  subnet_id     = var.subnet_id
   tags = {
     Name = var.instance-name
     application = var.app
   }
 }
 
-resource "aws_instance" "my-ec2-2" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name = "black-Bottle"
-  tags = {
-    Name = var.instance-name2
-    application = var.app2
-  }
-}
